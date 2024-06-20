c2d9c2c10b47e64c9140b4382d744b7352a50e53
Jiake Wang <149813501+RastyASWoz@users.noreply.github.com>
2024-06-20 11:06:16 +0800

    v1

---
+++ cal1.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+# 广义极角，theta = 2*pi*s/l l为边界的长度，s为边界上的点到起点的距离
+def get_thetas(curve_points):
+    # l = ∑(√((x_i+1-x_i)^2+(y_i+1-y_i)^2))
+    # s 为边界上的点到起点的距离
+    s = [0]
+    for i in range(1,len(curve_points),1):
+        s.append(s[i-1]+np.sqrt((curve_points[i][0]-curve_points[i-1][0])**2+(curve_points[i][1]-curve_points[i-1][1])**2))
+    s.append(s[-1]+np.sqrt((curve_points[0][0]-curve_points[-1][0])**2+(curve_points[0][1]-curve_points[-1][1])**2))
+    l = s[-1]
+    #print(l)
+    s = np.array(s)
+    thetas = (s / l) * 2 * np.pi
+    return thetas
+
+def get_a_ns(thetas,curve_points,n):
+    a_ns0 = []
+    a_ns1 = []
+    for i in range(-n,n+1,1):
+        if i == 0:
+            r_0 = np.array([0.0,0.0])
+            for j in range(0,len(curve_points),1):
+                r_j = curve_points[j]
+                theta_j = thetas[j]
+                if j == len(curve_points)-1:
+                    theta_j_1 = 2*np.pi
+                    r_j_1 = curve_points[0]
+                else:
+                    theta_j_1 = thetas[j+1]
+                    r_j_1 = curve_points[j+1]
+                r_0 += (r_j+r_j_1)/2 * (theta_j_1-theta_j)
+            r_0 /= (2*np.pi)
+            a_ns0.append(0)
+            a_ns1.append(0)
+        # a_n = 1/2pi * ∫(f(t) * e^(nj)dt)
+        else:
+            a_n0 = 0
+            a_n1 = 0
+            for j in range(0,len(curve_points),1):
+                r_j = curve_points[j]
+                theta_j = thetas[j]
+                if j == len(curve_points)-1:
+                    theta_j_1 = 2*np.pi
+                    r_j_1 = curve_points[0]
+                else:
+                    theta_j_1 = thetas[j+1]
+                    r_j_1 = curve_points[j+1]
+                rc_bar0 = (np.dot(r_j,[np.cos(i*theta_j),np.sin(i*theta_j)])+np.dot(r_j_1,[np.cos(i*theta_j_1),np.sin(i*theta_j_1)]))/2
+                rc_bar1 = (np.dot(r_j,[np.sin(i*theta_j),np.cos(i*theta_j)])+np.dot(r_j_1,[np.sin(i*theta_j_1),np.cos(i*theta_j_1)]))/2
+                a_n0 += rc_bar0 * (theta_j_1-theta_j)
+                a_n1 += rc_bar1 * (theta_j_1-theta_j)
+            a_n0 /= (2*np.pi)
+            a_n1 /= (2*np.pi)
+            a_ns0.append(a_n0)
+            a_ns1.append(a_n1)
+    return [a_ns0,a_ns1],r_0
+
+def F(r_0,theta,a_ns,n):
+    # F(θ) = ∑(a_n * e^(inθ))
+    r = np.array([0.0,0.0])
+    for i in range(-n,n+1,1):
+        r += a_ns[0][i+n] * np.array([np.cos(i*theta),np.sin(i*theta)]) + a_ns[1][i+n] * np.array([np.sin(i*theta),np.cos(i*theta)])
+    return r_0+r
---
+++ main.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import image
+import cal1
+
+image_path = 'img/3.jpeg'
+
+# 画出边界
+def plot_curve(curve_points):
+    plt.figure(figsize=(6,6))
+    plt.plot(curve_points[:,0],curve_points[:,1])
+    plt.show()
+    # for i in range(0,len(curve_points),50):
+    #     plt.scatter(curve_points[i][0],curve_points[i][1],c='r')
+    #     plt.draw()
+    #     plt.pause(0.1)
+
+# 画出傅里叶级数的结果
+def plot_res1( curve_points,n,mode=0):
+    thetas = cal1.get_thetas(curve_points)
+    a_ns,r_0 = cal1.get_a_ns(thetas,curve_points,n)
+    
+
+    # 生成一个θ的值的序列，例如从0到2π，步长为0.01
+    drawthetas = np.arange(0,2* np.pi, 0.01)
+
+    # 计算每个θ值对应的F(θ)的值
+    f_values = [cal1.F(r_0,theta, a_ns, n) for theta in drawthetas]
+    f_values = np.array(f_values)
+
+    # 使用matplotlib的plot函数绘制图像
+    plt.figure(figsize=(6,8))
+    plt.plot(curve_points[:,0],curve_points[:,1])
+    plt.plot(f_values[:,0],f_values[:,1])
+    
+    plt.title('n = ' + str(n))
+    if(mode == 0):
+        #plt.savefig('out/41f'+str(n)+'.jpg')
+        plt.show()
+    # else是为了展示当n增大时的变化
+    else:
+        plt.draw()
+        plt.pause(0.1)
+        plt.clf()
+
+curve_points = image.get_curve_points(image_path)
+plot_curve(curve_points)
+curve_points = np.array(curve_points)
+plot_res1(curve_points,50,0)
