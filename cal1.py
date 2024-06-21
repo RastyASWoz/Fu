b79db036e050afac207d3ee4cf5c1c34c68620ff
Zjl37 <2693911885@qq.com>
2024-06-22 00:50:13 +0800

    remove redundant branches in get_a_ns

--- cal1.py
+++ cal1.py
@@ -5,15 +5,15 @@ def get_thetas(curve_points):
     """广义极角，theta = 2*pi*s/l l为边界的长度，s为边界上的点到起点的距离"""
     # l = ∑(√((x_{i+1} - x_i)^2 + (y_{i+1} - y_i)^2))
     # s 为边界上的点到起点的距离
-    s = [0]
-    for i in range(1, len(curve_points), 1):
-        s.append(s[i-1]+np.sqrt((curve_points[i][0]-curve_points[i-1]
-                 [0])**2+(curve_points[i][1]-curve_points[i-1][1])**2))
-    s.append(s[-1]+np.sqrt((curve_points[0][0]-curve_points[-1][0])
-             ** 2+(curve_points[0][1]-curve_points[-1][1])**2))
+
+    curve_points = np.append(curve_points, [curve_points[0]], axis=0)
+    x = curve_points[:, 0]
+    y = curve_points[:, 1]
+
+    ds = np.sqrt((x[1:] - x[:-1])**2 + (y[1:] - y[:-1])**2)
+    s = np.cumsum(ds)
+
     l = s[-1]
-    # print(l)
-    s = np.array(s)
     thetas = (s / l) * 2 * np.pi
     return thetas
 
@@ -23,48 +23,32 @@ def get_a_ns(thetas, curve_points, n):
     a_ns0 = []
     a_ns1 = []
     for i in range(-n, n+1, 1):
-        if i == 0:
-            r_0 = np.array([0.0, 0.0])
-            for j in range(0, len(curve_points), 1):
-                r_j = curve_points[j]
-                theta_j = thetas[j]
-                if j == len(curve_points)-1:
-                    theta_j_1 = 2*np.pi
-                    r_j_1 = curve_points[0]
-                else:
-                    theta_j_1 = thetas[j+1]
-                    r_j_1 = curve_points[j+1]
-                r_0 += (r_j+r_j_1)/2 * (theta_j_1-theta_j)
-            r_0 /= (2*np.pi)
-            a_ns0.append(0)
-            a_ns1.append(0)
-        # a_n = 1/2pi * ∫(f(t) * e^(nj)dt)
-        else:
-            a_n0 = 0
-            a_n1 = 0
-            for j in range(0, len(curve_points), 1):
-                r_j = curve_points[j]
-                theta_j = thetas[j]
-                if j == len(curve_points)-1:
-                    theta_j_1 = 2*np.pi
-                    r_j_1 = curve_points[0]
-                else:
-                    theta_j_1 = thetas[j+1]
-                    r_j_1 = curve_points[j+1]
-                rc_bar0 = (np.dot(r_j, [np.cos(i*theta_j), np.sin(i*theta_j)]) +
-                           np.dot(r_j_1, [np.cos(i*theta_j_1), np.sin(i*theta_j_1)]))/2
-                rc_bar1 = (np.dot(r_j, [np.sin(i*theta_j), np.cos(i*theta_j)]) +
-                           np.dot(r_j_1, [np.sin(i*theta_j_1), np.cos(i*theta_j_1)]))/2
-                a_n0 += rc_bar0 * (theta_j_1-theta_j)
-                a_n1 += rc_bar1 * (theta_j_1-theta_j)
-            a_n0 /= (2*np.pi)
-            a_n1 /= (2*np.pi)
-            a_ns0.append(a_n0)
-            a_ns1.append(a_n1)
-    return [a_ns0, a_ns1], r_0
+        a_n0 = 0
+        a_n1 = 0
+        for j in range(0, len(curve_points), 1):
+            r_j = curve_points[j]
+            theta_j = thetas[j]
+            if j == len(curve_points)-1:
+                theta_j_1 = 2*np.pi
+                r_j_1 = curve_points[0]
+            else:
+                theta_j_1 = thetas[j+1]
+                r_j_1 = curve_points[j+1]
+            rc_bar0 = (np.dot(r_j, [np.cos(i*theta_j), np.sin(i*theta_j)]) +
+                       np.dot(r_j_1, [np.cos(i*theta_j_1), np.sin(i*theta_j_1)]))/2
+            rc_bar1 = (np.dot(r_j, [np.sin(i*theta_j), np.cos(i*theta_j)]) +
+                       np.dot(r_j_1, [np.sin(i*theta_j_1), np.cos(i*theta_j_1)]))/2
+            a_n0 += rc_bar0 * (theta_j_1-theta_j)
+            a_n1 += rc_bar1 * (theta_j_1-theta_j)
+        a_n0 /= (2*np.pi)
+        a_n1 /= (2*np.pi)
+        a_ns0.append(a_n0)
+        a_ns1.append(a_n1)
+    # return [a_ns0, a_ns1], r_0
+    return [a_ns0, a_ns1]
 
 
-def F(r_0, theta, a_ns, n):
+def F(theta, a_ns, n):
     # F(θ) = ∑(a_n * e^(inθ))
     r = np.array([0.0, 0.0])
     acun = int((len(a_ns[0]) - 1) / 2)
@@ -72,4 +56,4 @@ def F(r_0, theta, a_ns, n):
         r += a_ns[0][i] * np.array([np.cos((i-acun)*theta), np.sin((i-acun)*theta)]) + \
             a_ns[1][i] * np.array([np.sin((i-acun)*theta),
                                   np.cos((i-acun)*theta)])
-    return r_0+r
+    return r
--- main.py
+++ main.py
@@ -24,14 +24,14 @@ def plot_res1(curve_points, n, mode=0):
     prof__td = 0
 
     thetas = cal1.get_thetas(curve_points)
-    a_ns, r_0 = cal1.get_a_ns(thetas, curve_points, n)
+    a_ns = cal1.get_a_ns(thetas, curve_points, n)
     # 生成一个θ的值的序列，例如从0到2π，步长为0.01
     drawthetas = np.arange(0, 2 * np.pi, 0.01)
     print(f'[i] 计算系数用时 {time.time() - prof__t0} 秒')
 
     # 使用matplotlib的plot函数绘制图像
     if (mode == 0):
-        f_values = [cal1.F(r_0, theta, a_ns, n) for theta in drawthetas]
+        f_values = [cal1.F(theta, a_ns, n) for theta in drawthetas]
         f_values = np.array(f_values)
 
         plt.figure(figsize=(6, 8))
@@ -44,8 +44,8 @@ def plot_res1(curve_points, n, mode=0):
     else:
         for i in range(2, n+1):
             prof__t0 = time.time()
-            
-            f_values = [cal1.F(r_0, theta, a_ns, i) for theta in drawthetas]
+
+            f_values = [cal1.F(theta, a_ns, i) for theta in drawthetas]
             f_values = np.array(f_values)
 
             prof__td += time.time() - prof__t0
